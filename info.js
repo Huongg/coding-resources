66b94cff8b75a581bbd844d9b9a76ca1555470ee
Huongg <32060364+Huongg@users.noreply.github.com>
2019-06-09 11:02:26 +0100

    set-up

---
+++ info.js
@@ -0,0 +1,75 @@
+const data = [
+   {
+      "name": "CODECADEDMY",	
+      "description": "Started my journey by doing the foundations of HTML and CSS from Codecademy. Codecademy focuses on teaching beginners. It offers a mix of fun lessons, practical exercises, and rewards for your progress. </br></br>The only problem I had is that I can get the solution very easily when stuck. I found that I lost a bit of motivation to fully understand the solution. It is kind of a passive learning for me</p>",
+      "image": "./images/codecademy.png",
+      "cta": "Check it out here:",
+      "url": "https://www.codecademy.com/learn"
+   },
+
+   {
+      "name": "CODE FIRST GIRLS",	
+      "description": "After discovering Codefirstgirls through one of their events, I applied for their 3-month introduction to web development course. The course runs once a week in the evening.</br> I was struggling for the first 2 weeks as there was so much new knowledge I needed to understand (github, git, commandline, how the internet works). </br></br> Having said that, it is not impossible to learn. It will be always hard when you learn something completely new. Just keep learning is the key and it will be rewarding at the end!",
+      "image": "./images/CFG.png",
+      "cta": "Check out their new courses:",
+      "url": "https://www.codefirstgirls.org.uk/courses-we-offer.html"
+   },
+
+   {
+      "name": "JAVASCRIPT THROUGH YOUTUBE",	
+      "description": "Even after few months coding, I found myself still struggling to write effective Javascript. Youtube is one of the great online resources to learn (because for me, its not just passive reading). </br></br>",
+      "image": "./images/javascript.png",
+      "cta": "My favourite learning channel:",
+      "url": "https://www.youtube.com/user/shiffman"
+   },
+
+   {
+      "name": "FREECODECAMP",	
+      "description": "freeCodeCamp (FCC) is another great online platform to learn coding as they provide free-form projects and testing during the tutorials. By this time I had already completed my codecademy and codefirstgirls lessons, so taking FCC tutorials gave me a different view on how to understand the code and new challenges. </br></br>",
+      "image": "./images/FCC.png",
+      "cta": "More coding tutorials:",
+      "url": "https://learn.freecodecamp.org/"
+   },
+
+   {
+      "name": "CODEBAR",	
+      "description": "Codebar is an amazing tech community that helps me with any technical problem, and where I can make friends with many like-minded people. It is amazing not just because you will get help from experienced coaches on the 1-1 (or 2-1) team, but also you'll learn their real experiences on problem solving. ",
+      "image": "./images/codebar-brighton.png",
+      "cta": "Weekly coding workshops:",
+      "url": "https://codebar.io/events"
+   },
+
+   {
+      "name": "CODEWARS",	
+      "description": "Great for challenges and problem solving skills - pa pow. It can be quite confusing when you first go on the codewars website. On the platform, you are a code warrior who will train, either by yourself or with others, on programming challenges known as katas.",
+      "image": "./images/codewars.png",
+      "cta": "Are you ready for some challenges? ->",
+      "url": "https://www.codewars.com/"
+   },
+
+   {
+      "name": "KATATHON",	
+      "description": "Super cool Saturday coding workshop with weekly themes of codewars and freeCodeCamp. As I mentioned from the very beginning, coding on my own was intimidating and not always enjoyable. Every time I got stuck with my code, my emotion immediately went down the hill. ",
+      "image": "./images/katathon.png",
+      "cta": "Free this Saturday? ->",
+      "url": "https://www.meetup.com/Codecademy-London/events/hxspdpyxlbxb/"
+   },
+
+   {
+      "name": "CHINGU COHORT",	
+      "description": "A build-to-learn project working with a remote team of developers around the world. If you want to experience how it's like to be working in a dev team, I'd recommend to join Chingu. Chingu motivates you to step out of your comfort zone, to learn something very new and how to work with other developers. ",
+      "image": "./images/chingu.png",
+      "cta": "New cohorts are coming soon:",
+      "url": "https://chingu.io/"
+   },
+
+   {
+      "name": "MEETUP EVENTS",
+      "description": "Coding all the time is good, but it's even better if you can share your story to other people or listening to their experience on the latest technologies and trends. I do enjoy going to a lot of talks. Just knowing that I'm not alone can give me the confidence to move forward in my development learning path.",
+      "image": "./images/talks.jpg",
+      "cta": "Check out more events on:",
+      "url": "https://www.meetup.com/"
+   },
+]
+
+export default data;
